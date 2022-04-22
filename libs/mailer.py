077bb38e532dbc9536e2526ac3b15d7a2b8a0cf2
cnsfeir <cnsfeir@uc.cl>
2022-04-22 08:26:56 -0400

    feat: Define Mailer as a class
    For consistency and modularity reasons

--- libs/mailer.py
+++ libs/mailer.py
@@ -6,32 +6,37 @@ load_dotenv()
 MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY')
 MAILGUN_DOMAIN = os.environ.get('MAILGUN_DOMAIN')
 
-def send_mail(send_to: str, variables: dict) -> None:
-    print('\n 💌 SENDING MAIL...')
-    data = _get_base_data(send_to)
-    _add_variables(data, variables)
+class Mailer():
 
-    requests.post(
-        url=f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
-        auth=("api", MAILGUN_API_KEY),
-        data=data
-    )
-    print(' ✅ DONE!')
+    @classmethod
+    def send_mail(cls, send_to: str, variables: dict) -> None:
+        print('\n 💌 SENDING MAIL...')
+        data = cls._get_base_data(send_to)
+        cls._add_variables(data, variables)
 
-def _get_base_data(send_to: str) -> dict:
-    return {
-        'to': send_to,
-        'template': 'superhero_template',
-        'from': 'LIDS Newsletter <cnsfeir@uc.cl>',
-        'subject': '[Liga Interdimensional de Superhéroes] Última Fecha',
-    }
+        requests.post(
+            url=f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
+            auth=("api", MAILGUN_API_KEY),
+            data=data
+        )
+        print(' ✅ DONE!')
 
-def _add_variables(data: dict, variables: dict) -> None:
-    header_variables = {}
-    for key, value in variables.items():
-        if 'iterable.' in key:
-            header_variables.update({key.replace('iterable.', ''): value})
-        else:
-            data[f'v:{key}'] = value
+    @staticmethod
+    def _get_base_data(send_to: str) -> dict:
+        return {
+            'to': send_to,
+            'template': 'superhero_template',
+            'from': 'LIDS Newsletter <cnsfeir@uc.cl>',
+            'subject': '[Liga Interdimensional de Superhéroes] Última Fecha',
+        }
 
-    data['h:X-Mailgun-Variables'] = json.dumps(header_variables)
+    @staticmethod
+    def _add_variables(data: dict, variables: dict) -> None:
+        header_variables = {}
+        for key, value in variables.items():
+            if 'iterable.' in key:
+                header_variables.update({key.replace('iterable.', ''): value})
+            else:
+                data[f'v:{key}'] = value
+
+        data['h:X-Mailgun-Variables'] = json.dumps(header_variables)
